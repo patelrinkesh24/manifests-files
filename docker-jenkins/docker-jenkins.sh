53d910a517a21575c4af8dea417ef6b698765dde
Rinkeshkumar Patel <50281621+patelrinkesh24@users.noreply.github.com>
2023-06-08 23:54:04 -0400

    Create docker-jenkins.sh

---
+++ docker-jenkins/docker-jenkins.sh
@@ -0,0 +1,26 @@
+pipeline {
+    agent any
+    stages {
+        stage("git clone"){
+            steps{
+               
+            }
+        }
+        
+        stage("Build Docker Image"){
+            steps {
+                script {
+              
+                }
+            }
+        }
+        
+        stage("Push Docker Image"){
+            steps {
+                script {
+                      
+                }
+            }
+        }
+    }
+}
