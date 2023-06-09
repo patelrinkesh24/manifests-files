aca4a0d78f4fdba492c97e33f1db9149ab736f31
Rinkeshkumar Patel <50281621+patelrinkesh24@users.noreply.github.com>
2023-06-08 23:18:14 -0400

    output.tf

---
+++ globalvars/output.tf
@@ -0,0 +1,12 @@
+# Default tags
+output "default_tags" {
+  value = {
+    "Owner" = "Rinkesh"
+    "App"   = "Patel"
+  }
+}
+
+# Prefix to identify resources
+output "prefix" {
+  value     = "assignment1"
+}
